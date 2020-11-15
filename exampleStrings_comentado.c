fe08d43bc05a2c19f788b543d808339496ba11b8
alvaroAsturias20 <74318890+alvaroAsturias20@users.noreply.github.com>
2020-11-15 20:52:01 +0100

    Add files via upload

---
+++ exampleStrings_comentado.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+char array1[] = "Foo" "bar"; 
+char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r', '\0' };
+ 
+enum { BUFFER_MAX_SIZE = 1024 };
+ /*Raw string litereal no es implementado para c sino para c++. Su implementacion sería --> const char* s1 = "Hello world";*/
+const char* s1 = R"foo( 
+Hello
+World
+)foo";
+const char* s2 = "\nHello\nWorld\n";
+
+void gets_example_func(void) {
+  char buf[BUFFER_MAX_SIZE];
+ 
+  if (fgets(buf, sizeof(buf), stdin) == NULL) {
+        return 1;/*Al tratarse de una funcion void, no retorna nada por lo que suprimiendo el return ya no obtendriamos un warning, tambien se puede añadir un exit(0) para
+		que se detenga la ejecución el programa, sería lo mas correcto*/
+  }
+  buf[strlen(buf) - 1] = '\0';
+}
+
+const char *get_dirname(const char *pathname) {
+  char *slash;
+  slash = strrchr(pathname, '/');
+  if (slash) {
+    *slash = '\0'; /* Undefined behavior */
+  }
+  return pathname;
+}
+ 
+
+void get_y_or_n(void) {  
+	char response[8];/*Se esta reservando memoria de más, pues solo se va a almacenar 1 caracter 'y' or 'n', por lo que con un [2] se reduce la memoria reservada*/
+
+	printf("Continue? [y] n: ");  
+	gets(response);/*gets no es seguro pues se puede producir un desbordamiento de buffer, por lo que se utiliza fgets para evitar esto fgets(response,sizeof(response),stdin);
+	al indicar la longitud, se previene el desbordamiento de buffer*/
+
+	if (response[0] == 'n') 
+		exit(0);  
+
+	return;
+}
+
+ 
+int main(int argc, char *argv[])
+{
+    char key[24];
+    char response[8];
+    char array3[16];
+    char array4[16];
+    char array5 []  = "01234567890123456";
+    char *ptr_char  = "new string literal";
+	char analitic3[100]="аналитик";
+    int size_array1 = strlen("аналитик");/*Pondría sizeof para guardar el tamaño del array en la variable size_array1*/
+    int size_array2 = sizeof(analitic3);/*Des esta forma, guardo el tamaño del array de analitic3 en lugar del*/
+    
+   // char analitic1[size_array1]="аналитик";
+   // char analitic2[size_array2]="аналитик";
+    
+
+    puts(get_dirname(__FILE__));
+
+        
+    strcpy(key, argv[1]);  
+    strcat(key, " = ");  
+    strcat(key, argv[2]);
+
+
+    fgets(response,sizeof(response),stdin);
+    
+    get_y_or_n();
+
+    printf ("%d",size_array1);/*Aqui podría imprimirse el valor de la longitud size_array1, para ello la sentencia debería ser printf ("%d",size_array1)*/
+    printf ("\n");
+    printf ("%d",size_array2);/*Aqui podría imprimirse el valor de la longitud size_array2, para ello la sentencia debería ser printf ("%d",size_array2)*/
+    printf ("\n");
+ 
+    puts (s1);
+    printf ("\n");
+    puts (s2);
+    printf ("\n");
+    
+    strncpy(array3, array5, sizeof(array3));  
+    strncpy(array4, array3, strlen(array3));
+    
+    array5 [0] = 'M';
+    ptr_char [0] = 'N';
+    
+    array3[sizeof(array3)-1]='\0';
+    
+    
+    return 0;
+}
